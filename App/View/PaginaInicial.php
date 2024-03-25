d2b306f2f7f5a9170a09249b76b7653dd48423c9
Marcelo <131005418+Profissional-Marcelo@users.noreply.github.com>
2024-03-25 12:52:13 -0300

    MVC

---
+++ App/View/PaginaInicial.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DINFO LOGIN - CONSULTA PATRIMONIO</title>
+</head>
+<body>
+    <section class="login" id="login">
+        <h1>DINFO</h1>
+        <div class="barra"></div>
+        <form action="form.php">
+            <input type="password" name="senha" id="senha" placeholder="Informe a senha">
+            <button type="submit">Entrar</button>
+            <button type="submit"></button>
+        </form>
+    </section>
+
+</body>
+</html>
